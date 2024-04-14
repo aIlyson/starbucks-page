9d17d001e202e04669f8fc9fc5efb7ce19e91325
aIlyson <alyssonmichel20@gmail.com>
2024-04-14 15:34:38 -0300

    Adjusted part "produtos"

--- cod/script.js
+++ cod/script.js
@@ -1,7 +1,7 @@
-$(document).ready(function(){
-  $('.galeria').slick({
+$(document).ready(function () {
+  $(".galeria").slick({
     centerMode: true,
-    centerPadding: '0%', // Espaço entre as imagens
+    centerPadding: "0%", // Espaço entre as imagens
     slidesToShow: 3, // Mostra três imagens
     responsive: [
       {
@@ -9,42 +9,58 @@ $(document).ready(function(){
         settings: {
           arrows: false,
           centerMode: true,
-          centerPadding: '10%', // Reduzindo o espaço entre as imagens para dispositivos menores
-          slidesToShow: 3
-        }
+          centerPadding: "10%", // Reduzindo o espaço entre as imagens para dispositivos menores
+          slidesToShow: 3,
+        },
       },
       {
         breakpoint: 480,
         settings: {
           arrows: false,
           centerMode: true,
-          centerPadding: '5%', // Reduzindo ainda mais o espaço para dispositivos menores
-          slidesToShow: 1
-        }
-      }
+          centerPadding: "5%", // Reduzindo ainda mais o espaço para dispositivos menores
+          slidesToShow: 1,
+        },
+      },
     ],
     prevArrow: false, // Remove o botão de navegação "Anterior"
-    nextArrow: false // Remove o botão de navegação "Próximo"
+    nextArrow: false, // Remove o botão de navegação "Próximo"
   });
 
   // Adiciona evento de clique para cada imagem
-  $('.galeria img').on('click', function(e){
-    const $carousel = $('.galeria');
+  $(".galeria img").on("click", function (e) {
+    const $carousel = $(".galeria");
     const clickX = e.pageX;
-    const widthBody = document.body.offsetWidth 
+    const widthBody = document.body.offsetWidth;
 
-    
     const isLeft = clickX < widthBody / 2;
 
-  
-
-
     if (isLeft) {
-      $carousel.slick('slickPrev'); // Avança para o slide anterior
+      $carousel.slick("slickPrev"); // Avança para o slide anterior
     } else {
-      $carousel.slick('slickNext'); // Avança para o próximo slide
+      $carousel.slick("slickNext"); // Avança para o próximo slide
     }
   });
 });
 
+// --------------------
 
+const qtyRemover = document.querySelectorAll(".qt-remover");
+const qtyAdicionar = document.querySelectorAll(".qt-adicionar");
+const qtyValor = document.querySelectorAll(".numero");
+
+for (let i = 0; i < qtyRemover.length; i++) {
+  const remover = qtyRemover[i];
+  const adicionar = qtyAdicionar[i];
+  const valor = qtyValor[i];
+
+  remover.addEventListener("click", () => {
+    const numero = parseInt(valor.innerText);
+    valor.innerText = Math.max(numero - 1, 0);
+  });
+
+  adicionar.addEventListener("click", () => {
+    const numero = parseInt(valor.innerText);
+    valor.innerText = Math.max(numero + 1, 0);
+  });
+}
